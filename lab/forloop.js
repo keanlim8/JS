09c0baf77d88ced5dd5118a9b4085eac42c6c79a
Kean <74170768+keanlim8@users.noreply.github.com>
2020-11-28 01:16:50 -0500

    Update forloop.js

--- lab/forloop.js
+++ lab/forloop.js
@@ -16,6 +16,9 @@ Treat 2 as an even number and 1 & 3 as odd
 
 For loops: https://csx.codesmith.io/units/precourse-part-2/challenge-for-loops-fundamentals*/
 
+//1. Write a for loop for the given output:
+//1, 3, 5, 7, 9
+//2, 4, 6, 8, 10
 var oddArray = [];
 
 for (var x=1; x<=10; x += 2) {
@@ -29,3 +32,14 @@ for (var y=2; y<=10; y += 2) {
   evenArray.push(y);
 }
 console.log(evenArray);
+
+//2. write a for loop that iterates between 0 and 10 and prints all even numbers.
+for (var x=0; x<=10; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");
+        }
+
+}
